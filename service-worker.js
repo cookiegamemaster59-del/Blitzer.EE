8f314984f7a5217cca7ab7eb2a2f480c6044aa3e
Agent001 <dev@back4app.com>
2025-10-27 21:01:56 +0000

    fix(admin+pwa): add PWA manifest/SW, restore admin modal and login call

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'blitzer-ee-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  } else {
+    e.respondWith(fetch(e.request).catch(() => caches.match('/index.html')));
+  }
+});
